5604bd1b6bd78751620da53554a49c3deb294bbc
Rajit-125 <rajithebbar125@gmail.com>
2023-11-04 12:04:53 +0530

    added new

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,14 +1,14 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
-import Admin from "../page/admin";
+import Navbar from "../page/admin";
 
 
 const router=createBrowserRouter(
     [
         {
-            path:"/admin",
-            element:<Admin/>,
+            path:"/",
+            element:<Navbar/>,
         },
     ],
 )
